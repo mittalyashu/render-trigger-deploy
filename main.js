aace133c44ae97d964e6e26db4808b77b62666f0
Yashu Mittal <mittalyashu77@gmail.com>
2020-06-12 17:23:27 +0530

    Trigger a deploy using deployHook as input to Render service

---
+++ main.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core');
+const exec = require('@actions/exec');
+
+const deployHook = core.getInput('deployHook', { required: true });
+
+try {
+  if (!deployHook) {
+    core.warning('deployHook is not provided.');
+  }
+
+  await exec.exec("curl - X POST - d '{}'", [deployHook]);
+}
+catch (err) {
+  core.setFailed(`Action failed with error ${err}`);
+}
