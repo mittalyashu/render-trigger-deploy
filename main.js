ebdfb77dd99d47f816c7349994698cab0e2d1f79
Yashu Mittal <mittalyashu77@gmail.com>
2020-06-12 17:40:56 +0530

    async func and console few statements

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
-const deployHook = core.getInput('deployHook', { required: true });
+const run = async () => {
+  try {
+    core.debug('Inside try block');
 
-try {
-  if (!deployHook) {
-    core.warning('deployHook is not provided.');
+    const deployHook = core.getInput('deployHook', { required: true });
+    console.log("deployHook");
+    console.log(deployHook);
+
+    console.log("core.isDebug()");
+    console.log(core.isDebug());
+
+    // log error if deployHook not provided
+    if (!deployHook) {
+      core.warning('deployHook is not provided.');
+    }
+
+    exec.exec("curl - X POST - d '{}'", [deployHook]);
   }
+  catch (err) {
+    core.setFailed(`Action failed with error ${err}`);
+  }
+}
 
-  exec.exec("curl - X POST - d '{}'", [deployHook]);
-}
-catch (err) {
-  core.setFailed(`Action failed with error ${err}`);
-}
+run()
